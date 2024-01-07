e9a9fafb404b1d1e10f01826cd0f5ade1fa93243
nithin <23c12.nithin@sjec.ac.in>
2024-01-07 08:36:21 +0530

    exe

--- web/static/time-ago.js
+++ web/static/time-ago.js
@@ -1,37 +1,35 @@
-class TimeAgo extends HTMLElement{
-    connectedCallback(){
+class TimeAgo extends HTMLElement {
+    connectedCallback() {
         const el = this.querySelector("time")
-        if(el ===null|| el.dateTime ==="" ){
-            return 
+        if (el === null || el.dateTime === "") {
+            return
         }
 
-        el.textContent = neatime(el.dateTime)
+        el.textContent = ago(el.dateTime)
     }
 }
 
-customElements.define("time-ago",TimeAgo)
+customElements.define("time-ago", TimeAgo)
 
-function neatime(text) {
-	var now = new Date(),
-		date = new Date(text),
-		diff = (now.getTime() - date.getTime()) / 1000;
-	if (diff <= 60) {
-		text = 'Just now';
-	}
-	else if ( (diff /= 60) < 60 ) {
-		text = (diff|0) + 'm ago';
-	}
-	else if ( (diff /= 60) < 24 ) {
-		text = (diff|0) + 'h ago';
-	}
-	else if ( (diff /= 24) < 7 ) {
-		text = (diff|0) + 'd ago';
-	}
-	else {
-		text = String(date).split(' ')[1] + ' ' + date.getDate();
-		if (diff>182 && now.getFullYear()!==date.getFullYear()) {
-			text += ', ' + date.getFullYear();
-		}
-	}
-	return text;
+function ago(text) {
+    const now = new Date()
+    const date = new Date(text)
+    let diff = (now.getTime() - date.getTime()) / 1000
+    if (diff <= 60) {
+        return 'Just now'
+    }
+    if ((diff /= 60) < 60) {
+        return (diff | 0) + 'm'
+    }
+    if ((diff /= 60) < 24) {
+        return (diff | 0) + 'h'
+    }
+    if ((diff /= 24) < 7) {
+        return (diff | 0) + 'd'
+    }
+    text = String(date).split(' ')[1] + ' ' + date.getDate()
+    if (diff > 182 && now.getFullYear() !== date.getFullYear()) {
+        return text + ', ' + date.getFullYear()
+    }
+    return text
 }
