98b361341cf8ed613d52911cb09c8b1c6e3d13cb
nearlynithin <nearlynithin@gmail.com>
2024-01-09 22:31:07 +0530

    try

--- cmd/lain/main.go
+++ cmd/lain/main.go
@@ -1,36 +1,23 @@
+// main.go
 package main
 
 import (
 	"context"
 	"database/sql"
 	"errors"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	_ "github.com/lib/pq"
 	"lain.sceptix.net"
 	"lain.sceptix.net/web"
 )
 
+var db *sql.DB
+
 func main() {
-
-	dsn := "postgresql://nithin:REVEAL_PASSWORD@sunset-worm-8099.8nk.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("failed to connect database", err)
-	}
-
-	var now time.Time
-	err = db.QueryRow("SELECT NOW()").Scan(&now)
-	if err != nil {
-		log.Fatal("failed to execute query", err)
-	}
-
-	fmt.Println(now)
 	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -38,35 +25,10 @@ func main() {
 }
 
 func run() error {
+	setupDB()
 
-	//The flag creation for the address
-	var (
-		addr       string
-		sqlAddr    string
-		sessionKey string
-	)
-
-	fs := flag.NewFlagSet("lain", flag.ExitOnError)
-	fs.StringVar(&addr, "addr", ":4000", "HTTP service address")
-	fs.StringVar(&sqlAddr, "sql-addr", "postgresql://root@PenPen:26257/defaultdb?sslmode=disable", "SQL address")
-	fs.StringVar(&sessionKey, "session-key", "2p5TgHr7qJzWs4vY", "Session Key")
-	if err := fs.Parse(os.Args[1:]); err != nil {
-		return fmt.Errorf("parse flags %w", err)
-	}
-
-	//making a database connection
-	db, err := sql.Open("postgres", sqlAddr)
-	if err != nil {
-		return fmt.Errorf("open db: %w", err)
-	}
-	defer db.Close()
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("ping db :%w", err)
-	}
-
-	if err := lain.MigrateSQL(context.Background(), db); err != nil {
-		return fmt.Errorf("migrate sql: %w", err)
-	}
+	// Vercel provides the PORT environment variable for the HTTP service address
+	addr := ":" + os.Getenv("PORT")
 
 	queries := lain.New(db)
 	svc := &lain.Service{
@@ -77,26 +39,44 @@ func run() error {
 	handler := &web.Handler{
 		Logger:     logger,
 		Service:    svc,
-		SessionKey: []byte(sessionKey),
+		SessionKey: []byte(os.Getenv("SESSION_KEY")), // Use environment variable for session key
+
 	}
 
-	//now creating a server and passing the Handler
+	// Creating an HTTP server
 	srv := &http.Server{
 		Handler: handler,
-		//The address we take that from the flag
-		Addr: addr,
+		Addr:    addr,
 	}
 
 	defer srv.Close()
 
-	//printing a log message here
+	// Printing a log message here
 	logger.Printf("listening on %s", addr)
 
-	err = srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
-		return fmt.Errorf("listen and serve error : %w", err)
+		return fmt.Errorf("listen and serve error: %w", err)
 	}
 
 	return nil
+}
 
+func setupDB() {
+	// Making a database connection
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("SQL_ADDR"))
+	if err != nil {
+		log.Fatal("failed to connect to database:", err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("failed to ping database:", err)
+	}
+
+	if err := lain.MigrateSQL(context.Background(), db); err != nil {
+		log.Fatal("failed to migrate SQL:", err)
+	}
 }
