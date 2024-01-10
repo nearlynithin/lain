8f5d2bb183ceb44579ba7d94498277ddef4cc1e8
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-01-10 10:48:49 +0530

    exe

--- web/static/time-ago.js
+++ web/static/time-ago.js
@@ -1,38 +1,9 @@
-class TimeAgo extends HTMLElement{
-    connectedCallback(){
-        const el = this.querySelector("time")
-        if(el ===null|| el.dateTime ==="" ){
-            return 
-        }
 
-        el.textContent = neatime(el.dateTime)
+    function submitForm() {
+        document.getElementById("post_id").submit();
     }
-}
-
-customElements.define("time-ago",TimeAgo)
-
-function neatime(text) {
-	var now = new Date(),
-		date = new Date(text),
-		diff = (now.getTime() - date.getTime()) / 1000;
-	if (diff <= 60) {
-		text = 'Just now';
-	}
-	else if ( (diff /= 60) < 60 ) {
-		text = (diff|0) + 'm ago';
-	}
-	else if ( (diff /= 60) < 24 ) {
-		text = (diff|0) + 'h ago';
-	}
-	else if ( (diff /= 24) < 7 ) {
-		text = (diff|0) + 'd ago';
-	}
-	else {
-		text = String(date).split(' ')[1] + ' ' + date.getDate();
-		if (diff>182 && now.getFullYear()!==date.getFullYear()) {
-			text += ', ' + date.getFullYear();
-		}
-	}console.log(text);
-	return text;
-    
-}
+    document.getElementById("comments-text").addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            submitForm();
+        }
+    });
